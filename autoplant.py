9c5c0337ec40b7e45a0cfe5c6406d9d41681591f
meichiii2003 <meichiii2003@gmail.com>
2025-02-12 23:34:52 +0800

    try

--- autoplant.py
+++ autoplant.py
@@ -186,11 +186,11 @@ if page == "🌦 Current Weather":
 
         # Fetch weather data
         #weather_url = f"{base_url}appid={api_key}&lat={lat}&lon={lon}&units=metric"
-        weather_url = f"{base_url}lat={lat}&lon={lon}&appid={api_key}&units=metric"
+        weather_url = f"{base_url}appid={api_key}&lat={lat}&lon={lon}"
         response = requests.get(weather_url)
         weather_data = response.json()
 
-        if weather_data.get("cod") == 200:
+        if weather_data.get("cod") == "200":
             main_data = weather_data["main"]
             current_temperature = main_data["temp"]
             current_humidity = main_data["humidity"]
@@ -198,7 +198,7 @@ if page == "🌦 Current Weather":
             weather_description = weather_data["weather"][0]["description"]
 
             st.write(f"**City:** {city_name}")
-            st.write(f"🌡 **Temperature:** {current_temperature:.2f}°C")
+            st.write(f"🌡 **Temperature:** {current_temperature:.2f} K")
             st.write(f"💧 **Humidity:** {current_humidity}%")
             st.write(f"💨 **Wind Speed:** {wind_speed} m/s")
             st.write(f"🌥 **Condition:** {weather_description}")
